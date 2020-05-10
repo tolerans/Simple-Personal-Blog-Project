acce2fa30a4a148f5398aac4baac3bd4515c58fe
tolerans <scriptkidceo@gmail.com>
2020-05-10 15:38:13 +0300

    Update.

---
+++ build/precache-manifest.405df104b8c1ec2e114cf62c7980f191.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d36bcb6b2a1a84a28935f07d04983602",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd0915a8a6d1a270b4fb",
+    "url": "/static/css/2.369bfc60.chunk.css"
+  },
+  {
+    "revision": "5f316ae2da3898223825",
+    "url": "/static/css/main.4c74dc66.chunk.css"
+  },
+  {
+    "revision": "cd0915a8a6d1a270b4fb",
+    "url": "/static/js/2.1a31f59f.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/static/js/2.1a31f59f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f316ae2da3898223825",
+    "url": "/static/js/main.ef7333a7.chunk.js"
+  },
+  {
+    "revision": "04643109dc2ab8ab916f",
+    "url": "/static/js/runtime-main.eb88af4e.js"
+  }
+]);
